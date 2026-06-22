baconpaul/clap-helpers

commit a0f6d2575d4d98610197cd5c383e2ebe49908500

Add support for flush_events

include/clap/helpers/host-proxy.hxx
@@ -45,6 +45,7 @@ namespace clap { namespace helpers {
       getExtension(_hostScratchMemory, CLAP_EXT_SCRATCH_MEMORY);
       getExtension(_hostMiniCurveDisplay, CLAP_EXT_MINI_CURVE_DISPLAY);
       getExtension(_hostWebview, CLAP_EXT_WEBVIEW);
+      getExtension(_hostFlushEvents, CLAP_EXT_FLUSH_EVENTS);
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
@@ -164,6 +165,20 @@ namespace clap { namespace helpers {
       std::ostringstream msg;
       msg << "Plugin called the method clap_host_" << method
           << "() on wrong thread! It must be called on audio thread!";
+      pluginMisbehaving(msg.str());
+   }
+
+   template <MisbehaviourHandler h, CheckingLevel l>
+   void HostProxy<h, l>::ensureNotAudioThread(const char *method) const noexcept {
+      if (l == CheckingLevel::None)
+         return;
+
+      if (!canUseThreadCheck() || isAudioThread())
+         return;
+
+      std::ostringstream msg;
+      msg << "Plugin called the method clap_host_" << method
+          << "() on wrong thread! It must NOT be called on audio thread!";
       pluginMisbehaving(msg.str());
    }
 
@@ -354,6 +369,7 @@ namespace clap { namespace helpers {
    template <MisbehaviourHandler h, CheckingLevel l>
    void HostProxy<h, l>::paramsRequestFlush() const noexcept {
       assert(canUseParams());
+      this->ensureNotAudioThread("params.request_flush");
       _hostParams->request_flush(_host);
    }
 
@@ -857,5 +873,20 @@ namespace clap { namespace helpers {
       assert(canUseWebview());
       this->ensureMainThread("webview.send");
       return _hostWebview->send(this->_host, buffer, size);
+   }
+
+   ////////////////////////////
+   // clap_host_flush_events //
+   ////////////////////////////
+   template <MisbehaviourHandler h, CheckingLevel l>
+   bool HostProxy<h, l>::canUseFlushEvents() const noexcept {
+      return _hostFlushEvents && _hostFlushEvents->request_flush;
+   }
+
+   template <MisbehaviourHandler h, CheckingLevel l>
+   void HostProxy<h, l>::flushEventsRequestFlush() const noexcept {
+      assert(canUseFlushEvents());
+      this->ensureNotAudioThread("flush_events.request_flush");
+      return _hostFlushEvents->request_flush(_host);
    }
 }} // namespace clap::helpers

include/clap/helpers/host-proxy.hh
@@ -222,21 +222,29 @@ namespace clap { namespace helpers {
       bool miniCurveDisplayGetHints(uint32_t kind,
                                     clap_mini_curve_display_curve_hints_t *hints) const noexcept;
 
-      ////////////////////////////
-      // nl_clap_plugin_webview //
-      ////////////////////////////
+      //////////////////////////////
+      // clap_host_plugin_webview //
+      //////////////////////////////
       bool canUseWebview() const noexcept;
       bool webviewSend(const void *buffer, uint32_t size) const noexcept;
+
+      ////////////////////////////
+      // clap_host_flush_events //
+      ////////////////////////////
+      bool canUseFlushEvents() const noexcept;
+      void flushEventsRequestFlush() const noexcept;
 
    protected:
       void ensureMainThread(const char *method) const noexcept;
       void ensureAudioThread(const char *method) const noexcept;
+      void ensureNotAudioThread(const char *method) const noexcept;
 
       const clap_host *const _host;
 
       const clap_host_audio_ports *_hostAudioPorts = nullptr;
       const clap_host_audio_ports_config *_hostAudioPortsConfig = nullptr;
       const clap_host_context_menu *_hostContextMenu = nullptr;
+      const clap_host_flush_events *_hostFlushEvents = nullptr;
       const clap_host_gui *_hostGui = nullptr;
       const clap_host_latency *_hostLatency = nullptr;
       const clap_host_log *_hostLog = nullptr;

include/clap/helpers/plugin.hh
@@ -220,6 +220,13 @@ namespace clap { namespace helpers {
       virtual bool implementsParamsOrigin() const noexcept { return false; }
       virtual bool paramsOriginGet(clap_id param_id, double *out_value) noexcept { return false; }
 
+      //--------------------------//
+      // clap_plugin_flush_events //
+      //--------------------------//
+      virtual bool implementsFlushEvents() const noexcept { return false; }
+      virtual void flushEventsFlush(const clap_input_events_t *in,
+                                    const clap_output_events_t *out) noexcept {}
+
       //----------------------------//
       // clap_plugin_remote_controls //
       //----------------------------//
@@ -408,7 +415,7 @@ namespace clap { namespace helpers {
       void checkParamThread() const noexcept;
       void ensureMainThread(const char *method) const noexcept;
       void ensureAudioThread(const char *method) const noexcept;
-      void ensureParamThread(const char *method) const noexcept;
+      void ensureFlushThread(const char *method) const noexcept;
 
       ////////////////////
       // General Checks //
@@ -450,6 +457,9 @@ namespace clap { namespace helpers {
 
    private:
       void ensureInitialized(const char *method) const noexcept;
+      bool checkValidFlushEvent(const clap_event_header *ev,
+                                size_t ev_index,
+                                const char *ext) const noexcept;
 
       /////////////////////
       // CLAP Interfaces //
@@ -579,10 +589,15 @@ namespace clap { namespace helpers {
                                                    uint32_t automation_state,
                                                    const clap_color_t *color) noexcept;
 
-      // clap_params_origin
+      // clap_plugin_params_origin
       static bool clapParamsOriginGet(const clap_plugin_t *plugin,
                                       clap_id param_id,
                                       double *out_value) noexcept;
+
+      // clap_plugin_flush_events
+      static void clapFlushEventsFlush(const clap_plugin_t *plugin,
+                                      const clap_input_events_t *in,
+                                      const clap_output_events_t *out) noexcept;
 
       // clap_plugin_remote_controls
       static uint32_t clapRemoteControlsPageCount(const clap_plugin *plugin) noexcept;
@@ -716,35 +731,36 @@ namespace clap { namespace helpers {
 
       // interfaces
       static const clap_plugin_audio_ports _pluginAudioPorts;
-      static const clap_plugin_audio_ports_config _pluginAudioPortsConfig;
       static const clap_plugin_audio_ports_activation _pluginAudioPortsActivation;
+      static const clap_plugin_audio_ports_config _pluginAudioPortsConfig;
       static const clap_plugin_configurable_audio_ports _pluginConfigurableAudioPorts;
-      static const clap_plugin_surround_t _pluginSurroundConfig;
+      static const clap_plugin_context_menu _pluginContextMenu;
+      static const clap_plugin_flush_events _pluginFlushEvent;
+      static const clap_plugin_gain_adjustment_metering _pluginGainAdjustmentMetering;
       static const clap_plugin_gui _pluginGui;
       static const clap_plugin_latency _pluginLatency;
+      static const clap_plugin_mini_curve_display _pluginMiniCurveDisplay;
       static const clap_plugin_note_name _pluginNoteName;
       static const clap_plugin_note_ports _pluginNotePorts;
-      static const clap_plugin_params _pluginParams;
       static const clap_plugin_param_indication _pluginParamIndication;
+      static const clap_plugin_params _pluginParams;
       static const clap_plugin_params_origin _pluginParamsOrigin;
       static const clap_plugin_posix_fd_support _pluginPosixFdSupport;
       static const clap_plugin_preset_load _pluginPresetLoad;
+      static const clap_plugin_project_location _pluginProjectLocation;
       static const clap_plugin_remote_controls _pluginRemoteControls;
       static const clap_plugin_render _pluginRender;
+      static const clap_plugin_resource_directory _pluginResourceDirectory;
       static const clap_plugin_state _pluginState;
       static const clap_plugin_state_context _pluginStateContext;
+      static const clap_plugin_surround _pluginSurroundConfig;
       static const clap_plugin_tail _pluginTail;
       static const clap_plugin_thread_pool _pluginThreadPool;
       static const clap_plugin_timer_support _pluginTimerSupport;
       static const clap_plugin_track_info _pluginTrackInfo;
-      static const clap_plugin_voice_info _pluginVoiceInfo;
-      static const clap_plugin_context_menu _pluginContextMenu;
-      static const clap_plugin_resource_directory _pluginResourceDirectory;
-      static const clap_plugin_undo_delta _pluginUndoDelta;
       static const clap_plugin_undo_context _pluginUndoContext;
-      static const clap_plugin_project_location _pluginProjectLocation;
-      static const clap_plugin_gain_adjustment_metering _pluginGainAdjustmentMetering;
-      static const clap_plugin_mini_curve_display _pluginMiniCurveDisplay;
+      static const clap_plugin_undo_delta _pluginUndoDelta;
+      static const clap_plugin_voice_info _pluginVoiceInfo;
       static const clap_plugin_webview _pluginWebview;
 
       // state

include/clap/helpers/plugin.hxx
@@ -149,6 +149,11 @@ namespace clap { namespace helpers {
    };
 
    template <MisbehaviourHandler h, CheckingLevel l>
+   const clap_plugin_flush_events Plugin<h, l>::_pluginFlushEvent = {
+      clapFlushEventsFlush,
+   };
+
+   template <MisbehaviourHandler h, CheckingLevel l>
    const clap_plugin_resource_directory Plugin<h, l>::_pluginResourceDirectory = {
       clapResourceDirectorySetDirectory,
       clapResourceDirectoryCollect,
@@ -578,6 +583,8 @@ namespace clap { namespace helpers {
             return &_pluginWebview;
          if (!strcmp(id, CLAP_EXT_PARAMS_ORIGIN) && self.implementsParamsOrigin())
             return &_pluginParamsOrigin;
+         if (!strcmp(id, CLAP_EXT_FLUSH_EVENTS) && self.implementsFlushEvents())
+            return &_pluginFlushEvent;
       }
 
       return nullptr;
@@ -1018,7 +1025,7 @@ namespace clap { namespace helpers {
                                       const clap_input_events *in,
                                       const clap_output_events *out) noexcept {
       auto &self = from(plugin);
-      self.ensureParamThread("clap_plugin_params.flush");
+      self.ensureFlushThread("clap_plugin_params.flush");
 
       if (l >= CheckingLevel::Maximal) {
          if (!in)
@@ -1028,14 +1035,8 @@ namespace clap { namespace helpers {
             uint32_t N = in->size(in);
             for (uint32_t i = 0; i < N; ++i) {
                auto ev = in->get(in, i);
-               if (!ev) {
-                  std::ostringstream msg;
-                  msg << "clap_plugin_params.flush called null event inside the input list at "
-                         "index: "
-                      << i;
-                  self.hostMisbehaving(msg.str());
+               if (!self.checkValidFlushEvent(ev, i, "clap_plugin_params"))
                   continue;
-               }
 
                const bool isCoreSpace = ev->space_id == CLAP_CORE_EVENT_SPACE_ID;
                const bool isParamEvent =
@@ -1049,27 +1050,6 @@ namespace clap { namespace helpers {
                          " event type from CLAP_CORE_EVENT_SPACE_ID.";
                   self.hostMisbehaving(msg.str());
                   continue;
-               }
-
-               auto pev = reinterpret_cast<const clap_event_param_value *>(ev);
-
-               if (self._host.canUseThreadCheck() && self._host.isMainThread() &&
-                   !self.isValidParamId(pev->param_id)) {
-                  std::ostringstream msg;
-                  msg << "clap_plugin_params.flush called unknown paramId: " << pev->param_id;
-                  self.hostMisbehaving(msg.str());
-                  continue;
-               }
-
-               clap_param_info info;
-               if (self.getParamInfoForParamId(pev->param_id, &info)) {
-                  if (pev->value < info.min_value || info.max_value < pev->value) {
-                     std::ostringstream msg;
-                     msg << "clap_plugin_params.flush() produced the value " << pev->value
-                         << " for parameter " << pev->param_id << " which is out of bounds: ["
-                         << info.min_value << " .. " << info.max_value << "]";
-                     self._host.pluginMisbehaving(msg.str());
-                  }
                }
             }
          }
@@ -1281,6 +1261,89 @@ namespace clap { namespace helpers {
       }
 
       return self.paramsOriginGet(param_id, out_value);
+   }
+
+   //--------------------------//
+   // clap_plugin_flush_events //
+   //--------------------------//
+   template <MisbehaviourHandler h, CheckingLevel l>
+   void Plugin<h, l>::clapFlushEventsFlush(const clap_plugin_t *plugin,
+                                           const clap_input_events_t *in,
+                                           const clap_output_events_t *out) noexcept {
+      auto &self = from(plugin);
+      self.ensureFlushThread("clap_plugin_flush_events.flush");
+
+      if (l >= CheckingLevel::Maximal) {
+         if (!in)
+            self.hostMisbehaving(
+               "clap_plugin_flush_events.flush called with an null input parameter change list");
+         else {
+            uint32_t N = in->size(in);
+            for (uint32_t i = 0; i < N; ++i) {
+               auto ev = in->get(in, i);
+               if (!self.checkValidFlushEvent(ev, i, "clap_plugin_flush_events"))
+                  continue;
+            }
+         }
+
+         if (!out)
+            self.hostMisbehaving(
+               "clap_plugin_flush_events.flush called with an null output parameter change list");
+      }
+
+      self.flushEventsFlush(in, out);
+   }
+
+   template <MisbehaviourHandler h, CheckingLevel l>
+   bool Plugin<h, l>::checkValidFlushEvent(const clap_event_header *ev,
+                                           size_t ev_index,
+                                           const char *ext) const noexcept {
+      if (l < CheckingLevel::Minimal)
+         return true;
+
+      if (!ev) {
+         std::ostringstream msg;
+         msg << ext
+             << ".flush called null event inside the input list at "
+                "index: "
+             << ev_index;
+         hostMisbehaving(msg.str());
+         return false;
+      }
+
+      switch (ev->space_id) {
+      case CLAP_CORE_EVENT_SPACE_ID: {
+         switch (ev->type) {
+         case CLAP_EVENT_PARAM_VALUE: // fallthrough
+         case CLAP_EVENT_PARAM_MOD: {
+            auto pev = reinterpret_cast<const clap_event_param_value *>(ev);
+
+            if (_host.canUseThreadCheck() && _host.isMainThread() &&
+                !isValidParamId(pev->param_id)) {
+               std::ostringstream msg;
+               msg << ext << ".flush called unknown paramId: " << pev->param_id;
+               hostMisbehaving(msg.str());
+               return false;
+            }
+
+            clap_param_info info;
+            if (getParamInfoForParamId(pev->param_id, &info)) {
+               if (pev->value < info.min_value || info.max_value < pev->value) {
+                  std::ostringstream msg;
+                  msg << ext << ".flush() produced the value " << pev->value << " for parameter "
+                      << pev->param_id << " which is out of bounds: [" << info.min_value << " .. "
+                      << info.max_value << "]";
+                  _host.pluginMisbehaving(msg.str());
+               }
+            }
+            break;
+         }
+         }
+         break;
+      }
+      }
+
+      return true;
    }
 
    //----------------------------//
@@ -2123,7 +2186,7 @@ namespace clap { namespace helpers {
    }
 
    template <MisbehaviourHandler h, CheckingLevel l>
-   void Plugin<h, l>::ensureParamThread(const char *method) const noexcept {
+   void Plugin<h, l>::ensureFlushThread(const char *method) const noexcept {
       if (l == CheckingLevel::None)
          return;
 

commit 16c744cb53c804bc7cd741b8b9e001e535b8040d

Fix a regression in ensureNotAudioThread
in a0f6d257 ensureNotAudioThread was added, but it was
missing the !, so it basically was an ensureAudioThread
check!

include/clap/helpers/host-proxy.hxx
@@ -175,7 +175,7 @@ namespace clap { namespace helpers {
       if (l == CheckingLevel::None)
          return;
 
-      if (!canUseThreadCheck() || isAudioThread())
+      if (!canUseThreadCheck() || !isAudioThread())
          return;
 
       std::ostringstream msg;